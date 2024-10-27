1845bafd60bed275274b16f9caab8becadae519d
Neha Patil <patilneha165@gmail.com>
2024-10-27 18:17:16 +0100

    python print values of list

---
+++ 123.py
@@ -0,0 +1,8 @@
+list1=[1,2,3,0,0,0,-9,-8,-7,8,-3,0]
+print(list1)
+list2=[value for value in list1 if value>0]
+print(list2)
+list3=[value for value in list1 if value<0]
+print(list3)
+list4=[value for value in list1 if value==0]
+print(list4)
